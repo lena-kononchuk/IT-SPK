76c0048770f78887a26cea2d2ba1b3c159cb1fd6
HakunaMatata <kononchuk.elena91@gmail.com>
2022-07-21 10:57:45 +0300

    finish project: popup1, spollers1, validation form(php), callback

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,11 +1,4 @@
 
-/*
-Документация по работе в шаблоне: https://www.lightgalleryjs.com/docs/
-Документация плагина: https://www.lightgalleryjs.com/docs/
-Сниппет(HTML):
-*/
-
-// Подключение функционала "Чертогов Фрилансера"
 import { isMobile, FLS } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
@@ -20,21 +13,6 @@ import lightGallery from 'lightgallery';
 
 // Базовые стили
 import '@scss/libs/gallery/lightgallery.scss';
-// Стили дополнений
-// import '@scss/libs/gallery/lg-thumbnail.scss';
-// import '@scss/libs/gallery/lg-video.scss';
-// import '@scss/libs/gallery/lg-autoplay.scss';
-// import '@scss/libs/gallery/lg-zoom.scss';
-// import '@scss/libs/gallery/lg-pager.scss';
-// import '@scss/libs/gallery/lg-fullscreen.scss';
-// import '@scss/libs/gallery/lg-share.scss';
-// import '@scss/libs/gallery/lg-comments.scss';s
-// import '@scss/libs/gallery/lg-rotate.scss';
-// import '@scss/libs/gallery/lg-medium-zoom.scss';
-// import '@scss/libs/gallery/lg-relative-caption.scss';
-
-// Все стили
-// import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
 const galleries = document.querySelectorAll('[data-gallery]');
